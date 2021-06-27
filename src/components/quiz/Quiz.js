ccd91b680be65ed9b8d22cbe9fd6b62e9c709a40
Alvin Paul L. Azurin <alvin_azurin@yahoo.com>
2021-06-27 22:11:55 +0800

    Used Mixin to shorten SCSS

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -1,6 +1,17 @@
+import { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+
 function Quiz() {
     return (
-        <p>Quiz</p>
+        <Fragment>
+            <Helmet>
+                <title>Quiz - Quick Quiz</title>
+            </Helmet>
+            <div id="quiz">
+                <section>
+                </section>
+            </div>
+        </Fragment>
     )
 }
 
